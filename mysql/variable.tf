d426a7766ad155b9d08b34bcff4229702dd546c2
mMeydan52 <nizamimeydan@gmail.com>
2022-06-06 19:38:38 +0000

    variables files

---
+++ mysql/variable.tf
@@ -0,0 +1,17 @@
+variable "rgrp_name" {
+  description = "Resource group name"
+  type        = string
+  default     = "my_rgrp"
+}
+
+variable "region" {
+  description = "Region"
+  type        = string
+  default     = "West Europe"
+}
+
+variable "vnet_name" {
+  description = "Region"
+  type        = string
+  default     = "West Europe"
+}
