5d5753ef366b0c57fe5fcb63db808698ea364880
Nizami <41498763+Meydan52@users.noreply.github.com>
2022-06-07 11:21:12 -0400

    output outlines

--- mysql/output.tf
+++ mysql/output.tf
@@ -1,3 +1,6 @@
 
-id - The ID of the MySQL Server.
-id - The ID of the MySQL Database.
+# id - The ID of the MySQL Server.
+
+# id - The ID of the MySQL Database.
+# id - The ID of the MySQL Firewall Rule.
+
