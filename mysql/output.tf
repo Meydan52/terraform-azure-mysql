dbaea933b223efd46fcc1220dda88d751f14a7d6
3latanova <zlatanovaz@gmail.com>
2022-06-07 16:27:52 +0000

    add outputs

--- mysql/output.tf
+++ mysql/output.tf
@@ -1,6 +1,15 @@
 
 # id - The ID of the MySQL Server.
+output "id_mysql_server" {
+    value = azurerm_mysql_server.mysql_server
+}
 
 # id - The ID of the MySQL Database.
+output "id_mysql_database" {
+    value = azurerm_mysql_database.mysql_db
+}
 # id - The ID of the MySQL Firewall Rule.
+output "id_mysql_firewall" {
+  value = azurerm_mysql_firewall_rule.ASG
+}
 
